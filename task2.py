9ae525f5867d8bb72d42e262d2406f76663df07e
Ivan Yevpat <maracasabat@gmail.com>
2022-07-14 16:15:46 +0300

    metaclass fixed

--- task2.py
+++ task2.py
@@ -1,11 +1,18 @@
 class Meta(type):
-    class_number = 0
+    children_number = 0
 
-    def __new__(mcs, name, bases, namespace, **kwargs):
-        instance = super().__new__(mcs, name, bases, namespace, **kwargs)
-        instance.class_number = mcs.class_number
-        mcs.class_number += 1
+    def __new__(mcs, name, bases, namespace, *kwargs):
+        instance = super().__new__(mcs, name, bases, namespace)
+        instance.class_number = mcs.children_number
+        mcs.children_number += 1
         return instance
+
+    @classmethod
+    def __prepare__(mcs, name, bases, *kwargs):
+        return super().__prepare__(name, bases)
+
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
 
 
 Meta.children_number = 0
@@ -21,8 +28,12 @@ class Cls2(metaclass=Meta):
         self.data = data
 
 
+class Cls3(metaclass=Meta):
+    def __init__(self, data):
+        self.data = data
+
+
 assert (Cls1.class_number, Cls2.class_number) == (0, 1)
-print(f'#1. Cls1 и Cls2: {Cls1.class_number}, {Cls2.class_number} ')
-a, b = Cls1(''), Cls2('')
+a, b, c = Cls1(''), Cls2(''), Cls3('')
 assert (a.class_number, b.class_number) == (0, 1)
-print(f'#2. Cls1 и Cls2: {a.class_number}, {b.class_number} ')
+# print({Meta.children_number})
